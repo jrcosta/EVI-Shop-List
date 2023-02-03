09376d986ecdcd5c1c8bb7276dd2293ddd0c805a
Eneri Junior <jr.icm.sc@gmail.com>
2023-02-03 02:38:37 -0300

    Ajustes pwa

--- scripts/script.js
+++ scripts/script.js
@@ -140,3 +140,27 @@ if ('serviceWorker' in navigator) {
     });
 }
 
+// Listen for the beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    showInstallPrompt();
+  });
+  
+  function showInstallPrompt() {
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+    });
+  }
+  
+
