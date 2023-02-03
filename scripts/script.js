a3d7da107da2f4ba6ecd6aa2b88ffab7a861ebd6
Eneri Junior <jr.icm.sc@gmail.com>
2023-02-03 03:05:37 -0300

    teste ajust pwa

--- scripts/script.js
+++ scripts/script.js
@@ -130,27 +130,45 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-// Listen for the beforeinstallprompt event
+let deferredPrompt; // Allows to show the install prompt
+let setupButton;
+
 window.addEventListener('beforeinstallprompt', (e) => {
     // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    showInstallPrompt();
-  });
-  
-  function showInstallPrompt() {
-    // Show the install prompt
+    console.log("beforeinstallprompt fired");
+    if (setupButton == undefined) {
+        setupButton = document.getElementById("setup_button");
+    }
+    // Show the setup button
+    setupButton.style.display = "inline";
+    setupButton.disabled = false;
+});
+
+function installApp() {
+    // Show the prompt
     deferredPrompt.prompt();
+    setupButton.disabled = true;
     // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the install prompt');
-      } else {
-        console.log('User dismissed the install prompt');
-      }
-    });
-  }
+    deferredPrompt.userChoice
+        .then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('PWA setup accepted');
+                // hide our user interface that shows our A2HS button
+                setupButton.style.display = 'none';
+            } else {
+                console.log('PWA setup rejected');
+            }
+            deferredPrompt = null;
+        });
+}
+
+
+window.addEventListener('appinstalled', (evt) => {
+    console.log("appinstalled fired", evt);
+});
+  
   
 
